967dbc0b8351d06f4ea269c4c5b44ecae18f6105
David_Lin <wcsodw1@gmail.com>
2023-07-07 10:31:39 +0800

    Remove OpenAIAPI

--- Choose_Best_TicketPrice.py
+++ Choose_Best_TicketPrice.py
@@ -10,7 +10,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import create_extraction_chain
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "YOUROPENAIAPI"
 OPENAI_LLM = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key = OPENAI_API_KEY)
 Ticket_Website = "https://www.expedia.com.tw/Flights-Search?leg1=from%3A%E5%8F%B0%E5%8C%97%20%28TPE-%E6%A1%83%E5%9C%92%E5%9C%8B%E9%9A%9B%E6%A9%9F%E5%A0%B4%29%2Cto%3A%E6%9B%BC%E8%B0%B7%20%28BKK%20-%20%E7%B4%A0%E8%90%AC%E9%82%A3%E6%99%AE%E5%9C%8B%E9%9A%9B%E6%A9%9F%E5%A0%B4%29%2Cdeparture%3A2023%2F7%2F12TANYT&leg2=from%3A%E6%9B%BC%E8%B0%B7%20%28BKK%20-%20%E7%B4%A0%E8%90%AC%E9%82%A3%E6%99%AE%E5%9C%8B%E9%9A%9B%E6%A9%9F%E5%A0%B4%29%2Cto%3A%E5%8F%B0%E5%8C%97%20%28TPE-%E6%A1%83%E5%9C%92%E5%9C%8B%E9%9A%9B%E6%A9%9F%E5%A0%B4%29%2Cdeparture%3A2023%2F7%2F13TANYT&mode=search&options=carrier%3A%2A%2Ccabinclass%3A%2Cmaxhops%3A1%2Cnopenalty%3AN&pageId=0&passengers=adults%3A1%2Cchildren%3A0%2Cinfantinlap%3AN&trip=roundtrip"
 
